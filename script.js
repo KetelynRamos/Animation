b33ce279281d7e237992cb8b8a7182c8a1f9106b
KetelynRamos <160733454+KetelynRamos@users.noreply.github.com>
2024-12-26 20:21:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-alert(teste)
 
-let list = document.querySelectorAll('.item1')
+
+let list = document.querySelectorAll('.item')
 let next = document.getElementById('next')
 let prev = document.getElementById('prev')
 
@@ -11,7 +11,7 @@ next.onclick = () =>{
     let activeOld = document.querySelector('.active')
     activeOld.classList.remove('active')
 
-    active = active>= count -1 ? 0 : active +1 
+    active = active >= count -1 ? 0 : active +1 
     list[active].classList.add('active')
 }
 
